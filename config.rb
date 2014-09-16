d52c03da81ab907c8cf3310983fbd2c9848d7a93
Alan Crissey <alan.crissey@mailchimp.com>
2014-09-16 16:46:12 -0400

    relative links

--- config.rb
+++ config.rb
@@ -15,18 +15,8 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :relative_assets
+  set :relative_links, true
 
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
